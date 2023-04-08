63efb6a70bb0d3cabcec7d1bf68e1068e577d4ab
heberaragao <hebert.aragao@outlook.com>
2023-04-07 23:08:37 -0300

    Primeiro Commit

---
+++ SegundoPrograma/SegundoPrograma/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var valorA = 10
+    var valorB = 20
+    var valorC = valorA * 5
+    println(valorA.times(5))
+    println(valorB.div(2))
+    println(valorB.plus(valorA))
+
+}
